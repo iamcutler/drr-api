1c9bf416acf960acf1a4c9a706ac531eedbbbb4b
Allan Cutler <musicfan03@gmail.com>
2013-12-30 23:06:08 -0800

    Function to setcorrect json content header and encode to json.

--- php_includes/api_class.php
+++ php_includes/api_class.php
@@ -4,4 +4,11 @@ class DRR_API {
   function __construct($connect) {
     $this->db = $connect;
   }
+  
+  // Output slim app with json content type
+  protected function toJSON($app, $data) {
+    $response = $app->response;
+    $response['Content-Type'] = 'application/json';
+    $response->body( json_encode($data) );
+  }
 }
