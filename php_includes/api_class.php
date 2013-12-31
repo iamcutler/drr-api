780ce76fc1b0d0957c5e1e2ddb4e24d845fa1a20
Allan Cutler <musicfan03@gmail.com>
2013-12-31 01:30:29 -0800

    Two new methods to fetch current dry users. Still tweaking for app specs

--- php_includes/api_class.php
+++ php_includes/api_class.php
@@ -5,6 +5,36 @@ class DRR_API {
     $this->db = $connect;
   }
   
+  public function get_users($app, $offset = 0, $max = 10) {
+    return ($offset != NULL && $max != NULL) ? $this->query_drr_users($app, $offset, $max) : $this->query_drr_users($app);
+  }
+  
+  // Get application users with pagination
+  protected function query_drr_users($app, $offset = 0, $max = 10) {
+    $results = $this->db->query("select 
+      users.name,
+      comm_users.thumb as thumbnail,
+      comm_users.alias as slug,
+      comm_users.status
+      from ".TABLE_PREFIX."_users as users, ".TABLE_PREFIX."_community_users as comm_users
+      where users.id = comm_users.userid
+      limit $offset, $max");
+    
+    // Set empty users array to push results
+    $users = [];
+    
+    // Format array for json output
+    foreach($results as $key => $val) {
+        $users[$key]["name"] = $val["name"];
+        $users[$key]["thumbnail"] = $val["thumbnail"];
+        $users[$key]["slug"] = $val["slug"];
+        $users[$key]["status"] = $val["status"];
+    }
+    
+    return $this->toJSON($app, $users);
+    $results->close();
+  }
+  
   // Output slim app with json content type
   protected function toJSON($app, $data) {
     $response = $app->response;
