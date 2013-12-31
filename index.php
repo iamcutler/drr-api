77e95fbed77144b5450e18388198d4efbf018482
Allan Cutler <musicfan03@gmail.com>
2013-12-31 01:29:04 -0800

    Refactored Slim instantiation to include settings. Added application mode. Tracked in config file.

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ require $_SERVER["DOCUMENT_ROOT"].'/php_includes/api_class.php';
 require 'vendor/slim/slim/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+  'mode' => APP_MODE
+));
 
 $app->get('/', function () {
     echo "Hello World";
